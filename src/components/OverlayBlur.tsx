17ca0019e9bb0c40a813c58bbc8330104cf845f6
erke31-2 <KaungSet>
2023-10-29 23:28:07 +0630

    adjust css

--- src/components/OverlayBlur.tsx
+++ src/components/OverlayBlur.tsx
@@ -3,15 +3,15 @@ import useGameStore from "../store/store";
 
 const OverlayBlur = () => {
   const roomId = useGameStore((state) => state.gameId);
-  const link = `https://connectfour-six.vercel.app/join-room?roomId=${roomId}`
+  const link = `https://connectfour-six.vercel.app/join-room?roomId=${roomId}`;
   const [hasCopied, setIsCopied] = useState(false);
   const copyText = () => {
-    navigator.clipboard.writeText(link).then(() => setIsCopied(true))
-  }
+    navigator.clipboard.writeText(link).then(() => setIsCopied(true));
+  };
 
   return (
-    <section className="fixed inset-0 flex justify-center items-center backdrop-blur-lg">
-      <article className="w-full max-w-[400px] shadow-overlayShadow p-6 flex justify-center items-center flex-col gap-y-4 rounded-lg bg-white/30">
+    <section className="fixed inset-0 flex justify-center items-center backdrop-blur-lg p-4">
+      <article className="w-full max-w-[400px] shadow-overlayShadow p-6 flex flex-col justify-center items-center  gap-y-4 rounded-lg bg-white/30">
         <span className="text-xl">Waiting for friend...</span>
         <p className="text-lg font-medium text-center">
           Share the Room Id or Link below with your friend to play.
@@ -19,11 +19,16 @@ const OverlayBlur = () => {
         <span className="bg-black text-white text-center py-2 px-4 tracking-wide rounded-md font-bold">
           {roomId}
         </span>
-        <div className="text-white flex flex-col items-end">
-          <button className="w-fit text-xs px-2 py-1 font-bold" onClick={copyText}>
+        <div className="text-white w-full flex flex-col">
+          <button
+            className="w-fit text-xs p-2 font-bold ml-auto"
+            onClick={copyText}
+          >
             {hasCopied ? "Copied!" : "Copy Link"}
           </button>
-          <p className="bg-black p-3 rounded-md whitespace-nowrap">{link}</p>
+          <div className="flex items-center whitespace-nowrap overflow-x-scroll bg-black p-3 rounded-md">
+            <p className="inline">{link}</p>
+          </div>
         </div>
       </article>
     </section>
